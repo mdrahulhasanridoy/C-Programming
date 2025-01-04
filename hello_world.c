4f8e4adcde0cb84b7369379fb3276e43f2c2894e
Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>
2025-01-04 09:13:44 +0600

    first program
    Signed-off-by: Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
-int main(){
-    //by using printf, we can print anything on screen
+#include <stdio.h>
+int main()
+{
+    // by using printf, we can print anything on screen
     printf("Hello World\n");
     return 0;
 }
