88048fe457b37828567736edd33e20c474626740
Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>
2025-01-05 19:48:52 +0600

    Unterstand Comment
    Signed-off-by: Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>

---
+++ comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // #Single-line-comments
+    // I am writing this just for fun. This line doesn't do anything
+    printf("I AM RIDOY\n");
+    /*
+    Program : Comments.c
+    #Multi-line-comments
+    */
+}
