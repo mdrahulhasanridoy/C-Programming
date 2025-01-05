8b60fdb450fe055f25aca0073be26f971605effe
Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>
2025-01-05 19:52:11 +0600

    rectangle area = length * width
    Signed-off-by: Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>

---
+++ area_of_rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int length, width;
+    printf("Enter length: ");
+    scanf("%d", &length);
+    printf("Enter width: ");
+    scanf("%d", &width);
+    printf("The area of this rectangle is %d\n", length * width);
+}
