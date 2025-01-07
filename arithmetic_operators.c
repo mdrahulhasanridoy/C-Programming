d728bf044d0d665adf49fdd679c4ee883dc6879f
Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>
2025-01-07 21:40:16 +0600

    plus
    Signed-off-by: Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>

---
+++ arithmetic_operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 3;
+    int c = a + b;
+    printf("Sum of %d and %d is %d\n", a, b, c);
+    printf("The remainder when a is divided by b is : %d\n", a % b);
+}
