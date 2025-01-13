1c0d6f5f20e3a948c80b7089680f3c63fd4d4cbe
Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>
2025-01-13 23:47:33 +0600

    if ( a = 11)
    Signed-off-by: Md. Rahul Hasan Ridoy <mdrahulhasanridoy@gmail.com>

---
+++ quix_if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    if (a = 11) //   = is assignment operator, == is comparison operator
+    {
+        printf("I am 11\n"); // This will be printed
+    }
+    else
+    {
+        printf("I am not 11\n");
+    }
+}
